3d66deb36376b8f61c36db602dd76b97a5af8a11
kowallus <kowallus@o2.pl>
2020-01-22 16:15:29 +0100

    Pivot based filter option for bf variants (+minor refactor)

--- PwHammDistAlgorithmFactory.cpp
+++ PwHammDistAlgorithmFactory.cpp
@@ -11,27 +11,27 @@ PwHammDistAlgorithm* getPwHammDistAlgorithmInstance(ExperimentParams &xParams) {
 }
 
 
-template<bool naive, bool nibble = false, bool grouped = false>
+template<bool shortcircuit, bool nibble = false, bool grouped = false, bool pivot = false>
 class BrutePwHammDistAlgorithmFactory: public PwHammDistAlgorithmFactory {
 public:
-    BrutePwHammDistAlgorithmFactory():PwHammDistAlgorithmFactory(string(nibble?"grouped ":"") + string(nibble?"nibble ":"") + string(naive?"naive ":"short-circuit ") + string("brute-force")) {};
+    BrutePwHammDistAlgorithmFactory():PwHammDistAlgorithmFactory(string(pivot?"pivot-based ":"") + string(grouped?"grouped ":"") + string(nibble?"nibble ":"") + string(shortcircuit?"short-circuit ":"naive ") + string("brute-force")) {};
 
     PwHammDistAlgorithm* getAlgorithmInstance(ExperimentParams &xParams) {
         if(xParams.isInBinaryMode())
-            return new BrutePwHammDistAlgorithm<naive, true>(xParams);
+            return new BrutePwHammDistAlgorithm<shortcircuit, true>(xParams);
         else if (nibble) {
             switch(xParams.bytesPerElement) {
-                case 1: return new NibbleBrutePwHammDistAlgorithm<naive, uint8_t>(xParams);
-                case 2: return new NibbleBrutePwHammDistAlgorithm<naive, uint16_t>(xParams);
+                case 1: return new NibbleBrutePwHammDistAlgorithm<shortcircuit, uint8_t>(xParams);
+                case 2: return new NibbleBrutePwHammDistAlgorithm<shortcircuit, uint16_t>(xParams);
                 default:
                     fprintf(stderr, "ERROR: unsupported bytes per element: %d.\n", (int) xParams.bytesPerElement);
                     exit(EXIT_FAILURE);
             }
         } else {
             switch(xParams.bytesPerElement) {
-                case 1: return new BrutePwHammDistAlgorithm<naive, false, uint8_t, grouped>(xParams);
-                case 2: return new BrutePwHammDistAlgorithm<naive, false, uint16_t, grouped>(xParams);
-                case 4: return new BrutePwHammDistAlgorithm<naive, false, uint32_t, grouped>(xParams);
+                case 1: return new BrutePwHammDistAlgorithm<shortcircuit, false, uint8_t, grouped, pivot>(xParams);
+                case 2: return new BrutePwHammDistAlgorithm<shortcircuit, false, uint16_t, grouped, pivot>(xParams);
+                case 4: return new BrutePwHammDistAlgorithm<shortcircuit, false, uint32_t, grouped, pivot>(xParams);
                 default:
                     fprintf(stderr, "ERROR: unsupported bytes per element: %d.\n", (int) xParams.bytesPerElement);
                     exit(EXIT_FAILURE);
@@ -49,8 +49,8 @@ public:
             fprintf(stderr, "Quantization unsupported for binary dataset.\n");
             exit(EXIT_FAILURE);
         } else {
-            PwHammDistAlgorithm* postAlgorithm = BrutePwHammDistAlgorithmFactory<true, true>().getAlgorithmInstance(xParams);
-            PwHammDistAlgorithmFactory* binaryAlgorithmFactory = new BrutePwHammDistAlgorithmFactory<false>();
+            PwHammDistAlgorithm* postAlgorithm = BrutePwHammDistAlgorithmFactory<false, true>().getAlgorithmInstance(xParams);
+            PwHammDistAlgorithmFactory* binaryAlgorithmFactory = new BrutePwHammDistAlgorithmFactory<true>();
             switch(xParams.bytesPerElement) {
                 case 1: return new QuantizationBasedPwHammDistAlgorithm<uint8_t>(xParams, new SimpleBinaryQuantizer<uint8_t>(), binaryAlgorithmFactory, postAlgorithm);
                 case 2: return new QuantizationBasedPwHammDistAlgorithm<uint16_t>(xParams, new SimpleBinaryQuantizer<uint16_t>(), binaryAlgorithmFactory, postAlgorithm);
@@ -63,16 +63,19 @@ public:
     }
 };
 
-
 map<string, PwHammDistAlgorithmFactory*> pwHammDistAlgorithmTypesMap =
-        {{NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true>()},
-         {SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false>()},
+        {{NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false>()},
+         {SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true>()},
          {QUATIZATION_BASED_FILTER_PWHD_ID, new QuantizationBasedPwHammDistAlgorithmFactory()},
-         {NIBBLE_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, true>()},
-         {NIBBLE_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, true>()},
-         {GROUPED_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, false, true>()},
-         {GROUPED_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, false, true>()},
-         {GROUPED_NIBBLE_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, true, true>()},
-         {GROUPED_NIBBLE_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, true, true>()}
+         {NIBBLE_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, true>()},
+         {NIBBLE_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, true>()},
+         {GROUPED_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, false, true>()},
+         {GROUPED_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, false, true>()},
+//         {GROUPED_NIBBLE_NAIVE_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<false, true, true>()},
+//         {GROUPED_NIBBLE_SHORT_CIRCUIT_BRUTE_FORCE_ID, new BrutePwHammDistAlgorithmFactory<true, true, true>()},
+         {PIVOT_FILTER_ID, new BrutePwHammDistAlgorithmFactory<false, false, false, true>()},
+         {SHORT_CIRCUIT_PIVOT_FILTER_ID, new BrutePwHammDistAlgorithmFactory<true, false, false, true>()},
+         {GROUPED_PIVOT_FILTER_ID, new BrutePwHammDistAlgorithmFactory<false, false, true, true>()},
+         {GROUPED_SHORT_CIRCUIT_PIVOT_FILTER_ID, new BrutePwHammDistAlgorithmFactory<true, false, true, true>()},
          };
 
