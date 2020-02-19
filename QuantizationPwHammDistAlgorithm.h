42158f81e3451bbfde19b679fd66a76777473ed0
kowallus <kowallus@o2.pl>
2020-02-19 11:28:56 +0100

    quantization using stats and column values intervals

--- QuantizationPwHammDistAlgorithm.h
+++ QuantizationPwHammDistAlgorithm.h
@@ -4,7 +4,8 @@
 #include "PwHammDistAlgorithmBase.h"
 
 const string SIMPLE_BINARY_QUANTIZER_ID = "sbq";
-const string THRESHOLD_STATS_BASED_QUANTIZER_ID = "tsbq";
+const string THRESHOLD_STATS_BASED_QUANTIZER_ID = "tbq";
+const string INTERVAL_STATS_BASED_QUANTIZER_ID = "ibq";
 
 const string QUATIZATION_BASED_FILTER_PWHD_ID = "qbf";
 
@@ -102,6 +103,9 @@ protected:
             end = (this->qxParams.m - end * 64) / 4;
             for(int shift = 0; shift < end; ++shift)
                 *tmp = ((*tmp) << 1) + ((*srcPtr64++) & 0x0001000100010001);
+        } else {
+            fprintf(stderr, "ERROR: unsupported bytes per element (%d) by simple quantization.\n", sizeof(uint));
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -159,11 +163,11 @@ class ThresholdStatsBasedBinaryQuantizer: public StatsBasedBinaryQuantizer<uint>
 protected:
 
     uint8_t* thresholds = 0;
-    uint16_t halfD;
+    uint16_t half;
 
     void initQuantizeRules(ExperimentParams &xParams) {
-        thresholds = new uint8_t[xParams.bytesPerSequence * sizeof(uint)];
-        halfD = xParams.d / 2 / this->SKIP_INTERVAL;
+        thresholds = new uint8_t[xParams.bytesPerSequence];
+        half = xParams.d / 2 / this->SKIP_INTERVAL;
     }
 
     void buildQuantizeRules(uint16_t begCol, uint16_t endCol) {
@@ -173,8 +177,8 @@ protected:
             uint16_t j;
             for(j = 0; j < this->sigma; j++) {
                 uint16_t newCumm = cumm + curColCounter[j];
-                if (newCumm >= halfD) {
-                    if (j && (newCumm - halfD > halfD - cumm))
+                if (newCumm >= half) {
+                    if (j && (newCumm - half > half - cumm))
                         j--;
                     break;
                 }
@@ -186,13 +190,13 @@ protected:
     }
 
     inline void quantizeSequence(uint8_t* dest, uint* src) {
-        uint16_t j = 0;
-        while(j < this->qxParams.m) {
-            for(uint8_t b = 0; b < 8; b += 4, j += 4) {
-                *dest += ((src[j] & this->bitMask) > thresholds[j]) << b;
-                *dest += ((src[j + 1] & this->bitMask) > thresholds[j + 1]) << (b + 1);
-                *dest += ((src[j + 2] & this->bitMask) > thresholds[j + 2]) << (b + 2);
-                *dest += ((src[j + 3] & this->bitMask) > thresholds[j + 3]) << (b + 3);
+        uint16_t i = 0;
+        while(i < this->qxParams.m) {
+            for(uint8_t b = 0; b < 8; b += 4, i += 4) {
+                *dest += ((src[i] & this->bitMask) > thresholds[i]) << b;
+                *dest += ((src[i + 1] & this->bitMask) > thresholds[i + 1]) << (b + 1);
+                *dest += ((src[i + 2] & this->bitMask) > thresholds[i + 2]) << (b + 2);
+                *dest += ((src[i + 3] & this->bitMask) > thresholds[i + 3]) << (b + 3);
             }
             ++dest;
         }
@@ -209,8 +213,90 @@ public:
     }
 
     string getName() { return THRESHOLD_STATS_BASED_QUANTIZER_ID + toString(this->testedBits); };
+};
+
+template<typename uint>
+class IntervalStatsBasedBinaryQuantizer: public StatsBasedBinaryQuantizer<uint> {
+protected:
+
+    pair<uint8_t, uint8_t>* intervals = 0;
+    uint16_t half;
+
+    void initQuantizeRules(ExperimentParams &xParams) {
+        intervals = new pair<uint8_t, uint8_t>[xParams.bytesPerSequence];
+        half = xParams.d / 2 / this->SKIP_INTERVAL;
+    }
+
+    void buildQuantizeRules(uint16_t begCol, uint16_t endCol) {
+        uint16_t* curColCounter = this->counter;
+
+        for(uint16_t i = begCol; i < endCol; i++) {
+            uint16_t cumm = 0;
+            uint16_t j;
+            uint16_t start = 0;
+            pair<uint8_t, uint8_t> interval(0, 0);
+            uint16_t min = half;
+            for(j = 0; j < this->sigma; j++) {
+                uint16_t newCumm = cumm + curColCounter[j];
+                if (newCumm >= half) {
+                    if (min > half - cumm) {
+                        min = half - cumm;
+                        interval.first = start;
+                        interval.second = j;
+                    }
+                    do {
+                        cumm = newCumm;
+                        newCumm -= curColCounter[start++];
+                    } while (start < j && newCumm > half);
+                    if (start <= j && min > cumm - half) {
+                        min = cumm - half;
+                        interval.first = start - 1;
+                        interval.second = j + 1;
+                    }
+                }
+                cumm = newCumm;
+            }
+            if (min > half - cumm) {
+                min = half - cumm;
+                interval.first = start;
+                interval.second = j;
+            }
+            intervals[i] = interval;
+            curColCounter += this->sigma;
+        }
+    }
+
+    inline void quantizeSequence(uint8_t* dest, uint* src) {
+        uint16_t i = 0;
+        while(i < this->qxParams.m) {
+            for(uint8_t b = 0; b < 8; b += 4, i += 4) {
+                const uint8_t tmp0 = src[i] & this->bitMask;
+                *dest += ((tmp0 >= intervals[i].first) && (tmp0 < intervals[i].second)) << b;
+                const uint8_t tmp1 = src[i + 1] & this->bitMask;
+                *dest += ((tmp1 >= intervals[i + 1].first) && (tmp1 < intervals[i + 1].second)) << (b + 1);
+                const uint8_t tmp2 = src[i + 2] & this->bitMask;
+                *dest += ((tmp2 >= intervals[i + 2].first) && (tmp2 < intervals[i + 2].second)) << (b + 2);
+                const uint8_t tmp3 = src[i + 3] & this->bitMask;
+                *dest += ((tmp3 >= intervals[i + 3].first) && (tmp3 < intervals[i + 3].second)) << (b + 3);
+            }
+            ++dest;
+        }
+    }
+
+public:
+    IntervalStatsBasedBinaryQuantizer(uint8_t testedBits) : StatsBasedBinaryQuantizer<uint>(testedBits) {}
+
+    virtual ~IntervalStatsBasedBinaryQuantizer() {
+        if (intervals) {
+            delete[] intervals;
+            intervals = 0;
+        }
+    }
+
+    string getName() { return INTERVAL_STATS_BASED_QUANTIZER_ID + toString(this->testedBits); };
 
 };
+
 
 template<typename uint>
 class QuantizationBasedPwHammDistAlgorithm : public PwHammDistAlgorithm {
